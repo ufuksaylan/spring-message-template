db5ea351178d0662a7574ff0ff6ecc38f9b88a79
ufuksaylan <ufuksaylannn@gmail.com>
2024-03-18 15:31:16 +0200

    add another origin

--- src/main/java/it/academy/message/MessageController.java
+++ src/main/java/it/academy/message/MessageController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin({"http://localhost:4200"})
+@CrossOrigin({"http://localhost:4200", "https://angular-message-template-zc5n.onrender.com"})
 @RequestMapping("messages")
 @AllArgsConstructor
 public class MessageController {
